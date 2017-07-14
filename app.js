9bcba3bb5a82b470f89174cdf780074257fcffe7
tripott <tripott@gmail.com>
2017-07-14 17:01:14 -0400

    added build-primary-key lib

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5555
 const dal = require('./dal.js')
+const { pathOr } = require('ramda')
+const buildPrimaryKey = require('./lib/build-primary-key')
 
 //list the Meds  using dal.listMeds(limit, cb function)
 app.get('/', (req, res, next) =>
@@ -11,8 +13,36 @@ app.get('/', (req, res, next) =>
     .send('Welcome to the Pharma API!  Try a call to GET /meds for starters.')
 )
 
+app.post('/meds', function(req, res, next) {
+  const med = pathOr(null, ['body'], req)
+
+  // MONDAY TO DO
+  // build a primary key generator function to create pk value: buildPrimaryKey(prefix, data)
+  // assoc (add) a primary key value (_id) to the med object
+  // assoc (add) a type property = "medication" to the med object
+
+  dal.createDoc(med, function(err, result) {
+    if (err) console.log(err)
+    res.status(201).send(result)
+  })
+})
+
 app.get('/meds', function(req, res, next) {
   dal.listMeds(10, function(err, data) {
+    if (err) console.log(err)
+    res.status(200).send(data)
+  })
+})
+
+app.get('/patients', function(req, res, next) {
+  dal.listPatients(10, function(err, data) {
+    if (err) console.log(err)
+    res.status(200).send(data)
+  })
+})
+
+app.get('/pharmacies', function(req, res, next) {
+  dal.listPharmacies(10, function(err, data) {
     if (err) console.log(err)
     res.status(200).send(data)
   })
--- dal.js
+++ dal.js
@@ -2,11 +2,45 @@ const PouchDB = require('pouchdb')
 PouchDB.plugin(require('pouchdb-find'))
 const db = new PouchDB(process.env.COUCHDB_URL + process.env.COUCHDB_NAME)
 
+function createDoc(doc, callback) {
+  console.log('createDoc', doc)
+
+  db.put(doc).then(res => callback(null, res)).catch(err => callback(err))
+}
+
 function listMeds(limit, callback) {
   find(
     {
       selector: {
         type: 'medication'
+      }
+    },
+    function(err, data) {
+      if (err) return callback(err)
+      callback(null, data.docs)
+    }
+  )
+}
+
+function listPatients(limit, callback) {
+  find(
+    {
+      selector: {
+        type: 'patient'
+      }
+    },
+    function(err, data) {
+      if (err) return callback(err)
+      callback(null, data.docs)
+    }
+  )
+}
+
+function listPharmacies(limit, callback) {
+  find(
+    {
+      selector: {
+        type: 'pharmacy'
       }
     },
     function(err, data) {
@@ -22,7 +56,10 @@ function find(query, cb) {
 }
 
 const dal = {
-  listMeds
+  createDoc,
+  listMeds,
+  listPatients,
+  listPharmacies
 }
 
 module.exports = dal
